0a6139763b039a214b12490982eef87b6894fb8b
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2024-07-03 17:39:00 -0400

    070324

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 function mostrar(){
-    document.getElementById('texto-oculto').style.display='block';
+  /*    document.getElementById('texto-oculto').style.display='block';*/
+  const textElement = document.getElementById('texto-oculto');
+
+  if (textElement.classList.contains('hidden')) {
+    textElement.classList.remove('hidden');
+    textElement.classList.add('visible');
+  } else {
+    textElement.classList.remove('visible');
+    textElement.classList.add('hidden');
+  }
 }

bab2d30846c02b60ee80548d57e598f6446d7022
CHELOIPO <iporre.marcelo.23@gmail.com>
2024-07-14 17:07:41 -0400

    14.07.2024

---
+++ includes/templades/header.php
@@ -0,0 +1,14 @@
+<header>
+    <input type="checkbox" id="menu-bar">
+    <label for="menu-bar" class="fa fa-bars"></label>
+    <a href=""><img src="../ConecBolivia2/imgenes/connectBolivialogo.png" width="180px" height="150" alt="LOGO" ></a> 
+    <nav class="navbar">
+        <a href="../../index.php">Home</a>
+        <a href="../../index.php #precios">Precios</a>
+        <a href="/index.php #detalles">Servicios</a>
+        <a href="#acerca_de ">Acerca de</a>
+        <a href="#opiniones">Opiniones</a>
+        <a href="#contactos">Contacto</a>
+        <a href="login.php">Login</a>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -10,22 +10,9 @@
     
   </head>
   <body>
-    <header>
-      
-      
-        <input type="checkbox" id="menu-bar">
-        <label for="menu-bar" class="fa fa-bars"></label>
-        <a href=""><img src="../ConecBolivia2/imgenes/connectBolivialogo.png" width="180px" height="150" alt="LOGO" ></a> 
-        <nav class="navbar">
-          <a href="">Home</a>
-          <a href="#precios">Precios</a>
-          <a href="#detalles">Servicios</a>
-          <a href="#acerca_de ">Acerca de</a>
-          <a href="#opiniones">Opiniones</a>
-          <a href="#contactos">Contacto</a>
-          <a href="login.php">Login</a>
-        </nav>
-    </header>
+  <?php
+    include './includes/templades/header.php';
+  ?>
     <section class="inicio">
       <div class="content">
         <h3>NET CONECTION <span>SPACE WEB </span></h3>

4da7a0936298299fe76c96c9652d3aa933ad0a54
CHELOIPO <iporre.marcelo.23@gmail.com>
2024-07-14 22:56:57 -0400

    14.07.2024.2300

--- includes/templades/header.php
+++ includes/templades/header.php
@@ -1,11 +1,20 @@
-<header>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../ConecBolivia2/css/style.css">
+    <title>Document</title>
+</head>
+
+
+<header >
     <input type="checkbox" id="menu-bar">
     <label for="menu-bar" class="fa fa-bars"></label>
     <a href=""><img src="../ConecBolivia2/imgenes/connectBolivialogo.png" width="180px" height="150" alt="LOGO" ></a> 
     <nav class="navbar">
-        <a href="../../index.php">Home</a>
-        <a href="../../index.php #precios">Precios</a>
-        <a href="/index.php #detalles">Servicios</a>
+        <a href="../../../ConecBolivia2/index.php">Home</a>
+        <a href="../../../ConecBolivia2/index.php/#precios">Precios</a>
+        <a href="../../../ConecBolivia2/index.php/#detalles">Servicios</a>
         <a href="#acerca_de ">Acerca de</a>
         <a href="#opiniones">Opiniones</a>
         <a href="#contactos">Contacto</a>
